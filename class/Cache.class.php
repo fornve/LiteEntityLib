d337249eeb3a3a42f88204f1f2f55ef8e80bdca5
Marek Dajnowski <marek@marek-laptop.(none)>
2009-07-13 23:39:27 +0100

    removed slashed from retrieved object & cache drived added (for memcache)

---
+++ class/Cache.class.php
@@ -0,0 +1,55 @@
+<?php
+
+	class Cache
+	{
+		private $cache = null;
+		
+		function __construct()
+		{
+			switch( CACHE_TYPE )
+			{
+				case 'memcache':
+				{
+					$this->cache = new Memcache();
+					
+					if( !$this->cache->connect( MEMCACHE_HOST, MEMCACHE_PORT ) )
+						$this->cache = null;
+						
+					break;
+				}
+
+				case 'disk':
+				case 'db':
+					return false;
+			}
+		}
+
+		function __destruct()
+		{
+			$this->close();
+		}
+
+		function set( $key, $var , $flag = null, $expire = null )
+		{
+			if( $this->cache )
+				$this->cache->set( $key, $var, $flag, $expire );
+		}
+
+		function get( $key, $flags = null )
+		{
+			if( $this->cache )
+				return $this->cache->get( $key, $flags );
+		}
+
+		function delete( $key, $timeout = null )
+		{
+			if( $this->cache )
+				$this->cache->delete( $key, $timeout );
+		}
+
+		function close()
+		{
+			if( $this->cache )
+				return $this->cache->close();
+		}
+	}
