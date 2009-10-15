ed2ed969e754c84181cc33d15d852b507e057793
Marek Dajnowski <tigi@sunforum.co.uk>
2009-10-15 15:56:22 +0000

    started pager

---
+++ class/Pager.class.php
@@ -0,0 +1,28 @@
+<?php
+
+class Pager
+{
+	public $max = 0;		// number of elements
+	public $elements = 10;	// elements on page
+	public $offset = 1;		// offset ( page )
+	public $self = '/';		// base url	
+	public $option = null;	// filter option
+	public $order = null;	// result order option
+
+	public $elements_loop = null; // read only
+
+	function __construct( $self, $max, $offset = 1 )
+	{
+		$this->self = $self;
+		$this->max = $max;
+		$this->offset = $offset;
+
+		$this->elements_loop = $this->elements + 1;
+	}
+
+	function CountPages()
+	{
+		return ceil( $this->max / $this->elements );
+	}
+
+}
