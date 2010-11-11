b09b12a3d3800f9a7ba90397dd98d436a5edfb71
Marek Dajnowski <marek@dajnowski.net>
2010-11-11 08:03:34 +0000

    BaseLanguage:: getInstance

--- class/BaseLanguage.class.php
+++ class/BaseLanguage.class.php
@@ -2,22 +2,26 @@
 	class BaseLanguage
 	{
 		public $languageCode = null;
-		
+
 		public function __construct()
 		{
 			$base = DefaultLanguage::DefaultContent();
+
 			foreach( $base as $key => $value )
 			{
 				$this->$key = $value;
 			}
 
 			$content = $this->Content();
+
 			if( $content ) foreach( $content as $key => $value )
 			{
 				$counter++;
 				
 				if( !$this->$key )
+				{
 					$this->$key = $value;
+				}
 				else
 				{
 					Filelog::Write( "Warning! Lang {$key} duplicated around line {$couner}." );
